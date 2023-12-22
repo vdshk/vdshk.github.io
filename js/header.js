8a377bb6ff2b82019089e48adb56f491115ddb6e
vadyushkins <vadim.i.abzalov@gmail.com>
2023-12-22 11:14:46 +0300

    Add Bridges Search algorithm

--- js/header.js
+++ js/header.js
@@ -1,9 +1,6 @@
 function updateHeader() {
     var header = document.querySelector("#header");
-    header.style.position = "absolute";
-    header.style.width = "960px";
     header.style.top = "0.7em";
-    header.style.left = "50%";
     header.style["margin-left"] = "auto";
     header.style["margin-right"] = "auto";
     header.style["text-align"] = "center";
